a2f68bd2f542996f575b5516cdb319d64fb306f6
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2023-02-28 13:41:48 -0300

    Inclusão de texto

--- script.js
+++ script.js
@@ -40,7 +40,16 @@ function atualizaInterface() {
   if(candidato.length > 0) {
     candidato = candidato[0];
     seuVotoPara.style.display = 'block';
+    aviso.style.display = 'block';
     descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
+
+    let fotosHtml = '';
+    for(let i in candidato.fotos) {
+      fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+    }
+
+    lateral.innerHTML = fotosHtml;
+
   }
 }
 
